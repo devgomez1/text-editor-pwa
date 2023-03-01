29ec5f7e471b73cba26c7ae723c64d6d51022e84
devin <devinelliotgomez@gmail.com>
2023-02-28 18:22:38 -0600

    finished todos in the src-sw.js as well as resolved some issues in json file

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,30 +1,49 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+// Import Workbox libraries
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
+// Pre-cache all files listed in the Webpack manifest
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Create a cache for HTML pages using the CacheFirst strategy
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // expire after 30 days
     }),
   ],
 });
 
+// Warm the pageCache by caching the homepage and index page
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// Register a route for navigation requests (i.e. clicking on links)
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+// Register a route for non-navigation requests (i.e. assets like stylesheets, scripts, and workers)
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new CacheFirst({
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60, // expire after 7 days
+      }),
+    ],
+  })
+);
